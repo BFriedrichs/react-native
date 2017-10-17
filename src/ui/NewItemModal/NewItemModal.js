22c04cd9c77e7afbb79f5ddac34cf292c801c798
Björn Friedrichs <bjoern@friedrichs1.de>
2017-10-17 23:44:48 +0200

    properly connected newItemModal

--- src/ui/NewItemModal/NewItemModal.js
+++ src/ui/NewItemModal/NewItemModal.js
@@ -2,15 +2,51 @@
 
 import React, { Component } from 'react';
 import { Text, TouchableHighlight, View } from 'react-native';
+import { Button, FormLabel, FormInput } from 'react-native-elements'
+import { connect } from 'react-redux'
 
-export default class NewItemModal extends Component {
+import Styles from './styles'
+
+import ItemActions from 'src/actions/ItemActions'
+import Colors from '../Colors'
+
+class NewItemModal extends Component {
+
+  static navigationOptions = {
+    title: 'New Item'
+  }
+
+  state = {
+      itemName: ''
+  }
+
   render() {
-    const { navigate } = this.props.navigation
+    const { goBack } = this.props.navigation
 
     return(
-      <View>
-        <Text>Add your Item</Text>
+      <View style={Styles.wrapper}>
+        <View style={Styles.nameInput} >
+          <FormLabel>Item Name</FormLabel>
+          <FormInput
+            onChangeText={(text) => this.setState({itemName: text})}
+            value={this.state.itemName}/>
+        </View>
+        <Button
+          large
+          icon={{name: 'check-circle'}}
+          backgroundColor={Colors.Blue}
+          title='SUBMIT'
+          disabled={this.state.itemName == '' ? true : false}
+          onPress={()=>{
+            this.props.dispatch(ItemActions.addItem(this.state.itemName))
+            goBack()
+          }}
+          />
       </View>
     )
   }
 }
+
+NewItemModal = connect()(NewItemModal)
+
+export default NewItemModal
---
+++ src/ui/NewItemModal/styles.js
@@ -0,0 +1,13 @@
+// @flow 
+
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  wrapper: {
+    
+  },
+  nameInput: {
+    marginBottom: 80
+  }
+})
